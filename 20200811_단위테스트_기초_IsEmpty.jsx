43191cff16d14b2131a8dc07e187bcb26522929a
Lee kang-yong <yebgi83@gmail.com>
2020-08-11 22:30:40 +0900

    Create 20200811_단위테스트_기초_IsEmpty.jsx

---
+++ 20200811_단위테스트_기초_IsEmpty.jsx
@@ -0,0 +1,5 @@
+// 아래의 함수를 가지고 jest를 활용한 단위 테스트를 만들어 보기로 하겠습니다.
+// 아래의 함수는 isEmpty 함수로, 인자로 받은 obj라는 변수가 값을 갖고 있으면 True 아니면 False를 리턴합니다.
+export default function isEmpty(obj) {
+    return !obj;
+}
